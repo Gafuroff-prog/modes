51b7437d4e2a5c60c2b6f3f24d782c333578c3e7
Gafuroff-prog <gafuroffgaming@gmail.com>
2025-01-18 15:09:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function lightMode(){
+    document.body.style.backgroundColor = '#C4B5A5';
+    document.body.style.color = 'black'
+}
+function darkMode(){
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white'
+}
